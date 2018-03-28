22760b973403cb6e31109a7d6521cca3f95f1ee5
andrewtatong <Andrewrendyanto99@gmail.com>
2018-03-28 16:47:57 +0700

    push

---
+++ andrewtatong/script.js
@@ -0,0 +1,5 @@
+alert("selamat datang di website ini");
+var nama = prompt("masukan nama");
+var alamat = prompt('alamat');
+alert('terima kasih nama: ' + nama + ' alamat: ' + alamat + ' karna telah mengunjungi web ini');
+confirm('kamu yakin akan masuk??'); 
